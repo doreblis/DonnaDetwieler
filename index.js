039f879ef6e83475a6aa27de42b46fa7228e87c5
Donna Detwieler <drdet7@gmail.com>
2019-06-12 19:42:46 -0500

    array

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const name = prompt('What is your name?');
+//examples of strings
+let seagull = "bird at sea";
+let seal = "ocean cat";
+let otter = "ocean cutie";
+let tiger = "big cat";
+let wolf = "hungry dog";
 
-const checkName = function (){
-    if(name === "") {
-        name = prompt('What is your name, for real this time?');
+console.log(seagull, seal, otter, tiger, wolf);
 
-    checkName(); //repeats until a name exists
-    }
-};
-checkName (); //kicks off the name-checking the first time
+//array of animals
+myArray = [];
 
-alert("Hello " + name + "!")
+//concatenation of string and variable
+let animalTalk = 'I went to the Zoo and saw a ' + seagull ;
+console.log(animalTalk);
+
+//creating an array of variables
+let myAnimalArray = [seagull, seal, otter, tiger, wolf];
+console.log(myAnimalArray);
+
+//array pop - removes from eng
+myAnimalArray.pop();
+console.log("pop", myAnimalArray);
+
+myAnimalArray.pop(seagull);
+console.log("pop tiger", myAnimalArray);
+
+//array push - adds to end of array
+myAnimalArray.push(wolf);
+console.log("push", myAnimalArray);
+
