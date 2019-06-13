a0eefc6b21d723dbac5e3ac8fc7152f5c5683795
Donna Detwieler <drdet7@gmail.com>
2019-06-12 19:15:36 -0500

    css styles

--- blog/index.js
+++ blog/index.js
@@ -1,12 +1,12 @@
 
 //this is a variable assigned to an element
 
-let imageElement = document.getElementById(BlogImg);
+let imageElement = document.getElementById('BlogImg');
 
 //this function named "alert function" which executes an alert
 let alertFunction = function() {
-  alert('Hello reader!')
+  alert('Hello reader!');
 };
 
 //this is assigning an event listener to your element in order to run a function
-imageElement.addEventListener("click", alertFunction);
+imageElement.addEventListener('click', alertFunction);
