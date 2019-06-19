01bd20da178440affb1ca45b570ddb9c91b018f2
Donna Detwieler <drdet7@gmail.com>
2019-06-18 22:37:48 -0500

    changes

--- contact/contact.js
+++ contact/contact.js
@@ -1,3 +1,13 @@
-const name = prompt("Hi there! What's your name?");
+const firstName = prompt("Hi there! What's your first name?");
+const lastName = prompt("Hi there! What's your last name?");
 const output = document.querySelector("#greeting");
-output.textContent = "Thanks for visiting, " + name + ".";
+
+if (firstName || lastName) {
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + "!</p>";
+} else {
+  output.innerHTML = "<p>Please tell us your first and last names!</p>";
+}
+}
+// If you want the message customized, do "<p id='testID'>" then add rules in css
+
+// output.textContent = "Thanks for visiting, " + name + ".";
