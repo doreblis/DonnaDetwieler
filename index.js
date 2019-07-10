782cc86dd7e6fd4e69dff56aec48b89ee5b83ce5
Donna Detwieler <drdet7@gmail.com>
2019-07-10 11:13:47 -0500

    arrays exercises

--- index.js
+++ index.js
@@ -30,5 +30,86 @@ document.querySelectorAll('nav li > a')
 
 render(states.Home);
 
-//upon clicking, we need to know what we clicked on and match that to its resepctive page content to be displayed
 
+let myNum = [1, 2, 3, 4, 5, 6,];
+let doubledNum = [];
+
+myNum.forEach(
+  function(num) {
+  doubledNum.push(num * 2);
+}
+);
+
+console.log("my numbers:", myNum, "//doubled:", doubledNum);
+
+
+let myNum2 = [1, 2, 3, 4, 5];
+let doubledNum2 = myNum2.map(num => num * 2);;
+
+console.log("doubled! mapped", doubledNum2);
+
+let myNum3 = [1, 2, 3, 4, 5, 6, 7];
+let oddNum = myNum3.filter(num => num % 2 !== 0);
+console.log("filtered! odd!", oddNum);
+
+
+let myNum4 = [1, 2, 3, 4, 5, 6, 7];
+let mySum = myNum4.reduce(
+  (prev, curr) => {
+    return curr + prev;
+  }
+);
+console.log("sum", mySum);
+
+
+
+let arrayValues = ["hi", "hello", "cat", "dog"]
+let arrayValues2 = ["his", "hellos", "cats", "dogs"]
+
+let arrayValues = ["hi", "hello", "hi", "cat", "hi", "dog"]
+let value = "hi";
+let value2 = "dog";
+
+let myNumbers = [1, 2, 3, 4, 5, 6, 7];
+
+//Write a function that takes an array of values and returns an boolean representing if the word "hello" exists in the array. HINT: test each element with a loop!
+function helloArray(strings) {
+
+
+
+
+}
+helloArray(arrayValues);
+helloArray(arrayValues2);
+
+//Write a function that takes an array of values and a target value and returns how many times that target value exists in the array.
+function countArray(strings, value) {
+
+
+
+
+}
+countArray(repeatValues, value);
+countArray(repeatValues, value2);
+
+
+//Write a function that takes an array and returns a new array containing only the values at odd indexes in that array.
+function oddIndexes(array) {
+
+
+
+
+}
+oddIndexes(arrayValues);
+oddIndexes(repeatValues);
+
+//Write a function called sumArray that takes an array of numbers and returns the sum of all of those numbers added together.
+function sumArray(numbers) {
+  
+  let sum = numbers.reduce ((x,y) => x + y);
+
+  return sum;
+
+}
+
+console.log("4", sumArray(myNumbers));
