ea28b64cbfce0392f76e77898dbd16f9e85b7f17
Donna Detwieler <drdet7@gmail.com>
2019-07-23 18:59:09 -0500

    navigo removed

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,44 +1,13 @@
 import { lowerCase } from 'lodash';
 
-function buildLinks(linkArray) { 
-    let link = "";
-    let links = "";
-    let x = 0;
-
-    linkArray.forEach(link => {
-        if (linkArray[x] !== "Home")
-        {
-            link = linkArray[x];
-          }
-      
-      
-          links += `
-                  <p>
-                      <a href='/${lowerCase(link)}' data-navigo>
-                          ${linkArray[x]}
-                      </a>
-                  </p>
-              `;
-      
-          x++;
-        });
-      
-        return links;
-      }
 
 export default function Navigation(state){ return `
 
-<nav id="nav">
-<p>
-    ${buildLinks(state.links)}
-</p>
 
-</nav>
-
-<div class="btn-group" role="group" aria-label="Basic example">
+<div class="nav" role="group" aria-label="Basic example">
   <button type="button" class="btn btn-secondary">Home</button>
-  <button type="button" class="btn btn-secondary">Middle</button>
-  <button type="button" class="btn btn-secondary">Right</button>
+  <button type="button" class="btn btn-secondary">Contact</button>
+  <button type="button" class="btn btn-secondary">Portfolio</button>
 </div>
 `;
 }
